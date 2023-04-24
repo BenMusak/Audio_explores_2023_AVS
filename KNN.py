f3a47ce7cfaf9829f1957cc7336bf1812e3b1272
BenMusak <56428534+BenMusak@users.noreply.github.com>
2023-04-24 02:36:58 +0200

    Added accuracy score

--- KNN.py
+++ KNN.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
 
 def knn_model(x_train, x_test, x_val, y_train, y_test, y_val):
@@ -30,14 +31,20 @@ def knn_model(x_train, x_test, x_val, y_train, y_test, y_val):
     model = GridSearchCV(KNeighborsClassifier(), grid_params, cv=5, n_jobs=-1)
     model.fit(x_train_scaled, y_train)
 
-    print(f'Model Score Training Data: {model.score(x_test_scaled, y_test)}')
-
     y_predict = model.predict(x_test_scaled)
-    print(f'Confusion Matrix for training data: \n{confusion_matrix(y_predict, y_test)}')
+    print(f'Model Score Test Data: {model.score(x_test_scaled, y_test)}')
+    print(f'Accuracy Score for Test Data: {accuracy_score(y_predict, y_test)}')
+    print(f'Confusion Matrix for Test Data: \n{confusion_matrix(y_predict, y_test)}')
 
     val_predict = model.predict(x_val)
     print(f'Model Score Validation Data: {model.score(x_val_scaled, y_val)}')
-    print(f'Confusion Matrix for Validation data: \n{confusion_matrix(val_predict, y_val)}')
+    print(f'Accuracy Score for Validation Data: {accuracy_score(val_predict, y_val)}')
+    print(f'Confusion Matrix for Validation Data: \n{confusion_matrix(val_predict, y_val)}')
+    
+    train_predict = model.predict(x_train)
+    print(f'Model Score Train Data: {model.score(x_train_scaled, y_train)}')
+    print(f'Accuracy Score for Train Data: {accuracy_score(train_predict, y_train)}')
+    print(f'Confusion Matrix for Train Data: \n{confusion_matrix(train_predict, y_train)}')
     
     start = time.time()
     print(f'Predicted label: {model.predict(x_test_scaled[0].reshape(1, -1))}')
